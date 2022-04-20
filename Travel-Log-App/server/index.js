3984937adbe96f34788bd6fabd1fa3d3d81e7da2
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-04-20 20:50:39 +0900

    [sv] add routes file

--- Travel-Log-App/server/index.js
+++ Travel-Log-App/server/index.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
 
+import travelLogRoutes from "./routes/travelLog.js";
+
 const app = express();
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello world");
-});
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.use("/travel-log", travelLogRoutes);
 
 const port = process.env.PORT || 5000;
 
