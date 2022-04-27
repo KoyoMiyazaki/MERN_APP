d0e196664b3c5d7658c9c95dbfeafe01b1cbe55c
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-04-27 13:54:20 +0900

    [sv] add controllers and models

--- Travel-Log-App/server/routes/travelLog.js
+++ Travel-Log-App/server/routes/travelLog.js
@@ -1,9 +1,9 @@
 import express from "express";
 
+import { getAllTravelLogs } from "../controllers/travelLog.js";
+
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.status(200).send("Travel Log App!");
-});
+router.get("/", getAllTravelLogs);
 
 export default router;
