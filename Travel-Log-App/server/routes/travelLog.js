a24fef3b2749aae82835cf3ac665b3f54de5d9f8
KoyoMiyazaki <70s.punkrock.development@gmail.com>
2022-04-28 23:50:52 +0900

    [sv] update controllers and models

--- Travel-Log-App/server/routes/travelLog.js
+++ Travel-Log-App/server/routes/travelLog.js
@@ -1,9 +1,19 @@
 import express from "express";
 
-import { getAllTravelLogs } from "../controllers/travelLog.js";
+import {
+  getAllTravelLogs,
+  createTravelLog,
+  getSingleTravelLog,
+  updateTravelLog,
+  deleteTravelLog,
+} from "../controllers/travelLog.js";
 
 const router = express.Router();
 
 router.get("/", getAllTravelLogs);
+router.post("/", createTravelLog);
+router.get("/:id", getSingleTravelLog);
+router.patch("/:id", updateTravelLog);
+router.delete("/:id", deleteTravelLog);
 
 export default router;
